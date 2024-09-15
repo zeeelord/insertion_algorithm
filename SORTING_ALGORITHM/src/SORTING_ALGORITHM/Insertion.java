7887820dc0dacf5894cbf63c297524af66d35260
AFOLABI_OLADAYO_DAVID <47390991+zeeelord@users.noreply.github.com>
2024-09-16 00:31:08 +0100

    Update Insertion.java

--- SORTING_ALGORITHM/src/SORTING_ALGORITHM/Insertion.java
+++ SORTING_ALGORITHM/src/SORTING_ALGORITHM/Insertion.java
@@ -11,7 +11,7 @@ public class Insertion {
 			int key = arr[i];
 			int j= i-1;
 			
-		    while(arr[j]>key && j>=0  ) {
+		    while(j>=0  && arr[j]>key  ) {
 			   
 			   arr[j+1]= arr[j];
 			   j-- ;
